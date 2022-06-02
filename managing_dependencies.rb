02307954a2344880917d66ab52cc4dfb526556d8
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-06-02 16:18:56 -0700

    change direction dependencies

--- managing_dependencies.rb
+++ managing_dependencies.rb
@@ -93,3 +93,55 @@ end
 puts Gear.new(52, 11, 26, 1.5).gear_inches
 
 
+
+# solution #3 change direction dependency
+
+
+class Gear
+
+  attr_reader :chainring, :cog
+
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+
+  end
+
+  def gear_inches(diameter)
+    ratio * wheel.diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+end
+
+
+class Wheel
+  attr_reader :rim, :tire, :gear
+
+  def initialize(rim:, tire:, chainring:, cog:)
+    @rim = rim
+    @tire = tire
+    @gear = Gear.new(chainring: chainring, cog: cog)
+  end
+
+  def diameter
+    rim * (tire * 2)
+  end
+
+  def gear_inches
+    gear.gear_inches(diameter)
+  end
+end
+
+
+puts Wheel.new(
+  rim:       26,
+  tire:      1.5,
+  chainring: 51,
+  cog:       11).gear_inches
+
+
+
