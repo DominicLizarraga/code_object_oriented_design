ff3278c3ca48e1dbaf22b5663b153b8ccc56be49
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-06-02 16:12:12 -0700

    isolate instances

--- managing_dependencies.rb
+++ managing_dependencies.rb
@@ -41,7 +41,7 @@ end
 puts Gear.new(52, 11, 26, 1.5).gear_inches
 
 
-# solution #1
+# solution #1 dependency injection
 
 class Gear
 
@@ -65,4 +65,31 @@ class Gear
 end
 
 puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
+
+
+# solution #2 isolate instances
+
+class Gear
+
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+    @tire = tire
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
+  end
+
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+
 
