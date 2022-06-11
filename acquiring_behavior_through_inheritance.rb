08e4cae116d69a8e2caeaee5223360d5389ebdb7
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-06-11 16:07:22 -0700

    template method pattern

--- acquiring_behavior_through_inheritance.rb
+++ acquiring_behavior_through_inheritance.rb
@@ -206,13 +206,98 @@ puts mountain_bike.spares # it will throw no super class method
 
 
 
+# fifth method to solve problems of related types that share behavior
+
+# this is the method called implementig template method
+
+class Bicycle
+
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(**opt)
+    @size = opts[:size]
+    @chain = opts[:chain]
+    @tire_size = opts[:tire_size]
+  end
+
+  def spares
+    { tire_size: tire_size,
+      chain:     chain
+    }
+  end
+
+  def default_chain
+    '11-speed'
+  end
+
+  def default_tire_size
+    raise NoImplementedError
+  end
+end
+
+class RoadBike < Bicycle
+
+  attr_reader :tape_color
+
+  def initialize(**opts)
+    @tape_color = opts[:tape_color]
+    super
+  end
+
+  def spares
+    super.merge(tape_color: tape_color)
+  end
+
+  def default_tire_size
+    '23'
+  end
+
+end
+
+class MountainBike
+
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(**opts)
+    @front_shock = front_shock
+    @rear_shock = rear_shock
+    super
+  end
+
+  def spares
+    super.merge(front_shock: front_shock)
+  end
+
+  def default_tire_size
+    '2.1'
+  end
 
 
+end
 
+# in case a new class must be added there would be no problem if programmer forgets super
 
+class RecumbentBike < Bicycle
 
+  attr_reader :flag
 
+  def initiliaze(**opts)
+    flag = opts[:flag]
+  end
 
+  def spares
+    super.merge(flag: flag)
+  end
 
+  def default_chain
+    '10-speed'
+  end
 
+  def default_tire_size
+    '28'
+  end
+end
+
+bent = RecumbentBike.new(flag: 'tall and orage')
+puts bent.spares
 
