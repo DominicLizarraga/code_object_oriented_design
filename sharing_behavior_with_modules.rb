95ef0fd44ecd4fba724d107c50dbe73663758145
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-06-15 23:26:42 -0700

    adding module

--- sharing_behavior_with_modules.rb
+++ sharing_behavior_with_modules.rb
@@ -54,5 +54,30 @@ puts b.schedulable?(starting, ending)
 # True
 
 
+# implementing first time the module to share behaviour
 
+
+module Schedulable
+
+  attr_reader :schedule
+
+  def schedule
+    @schedule ||= Schedule.new
+  end
+
+  def schedulable?(starting, ending)
+    :scheduled?(starting - lead_days, ending)
+  end
+
+  def scheduled?(starting, ending)
+    schedule.scheduled?(self, starting, ending)
+  end
+
+  def lead_days
+    0
+  end
+
+
+
+end
 
