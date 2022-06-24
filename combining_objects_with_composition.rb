89b94e6bbbd9ec2498e2123376c18ac9511dcd8f
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-06-23 21:07:08 -0700

    using module to create parts

--- combining_objects_with_composition.rb
+++ combining_objects_with_composition.rb
@@ -234,7 +234,37 @@ puts combo_parts.class         # => Array
 
 
 
+# 2nd attempt now using module
 
 
+road_config =
+  [['chain',      '11-speed'],
+   ['tire_size',  '23'],
+   ['tape_color', 'red']]
 
+mountain_config =
+  [['chain',       '11-speed'],
+   ['tire_size',   '2.1'],
+   ['front_shock', 'Manitou'],
+   ['rear_shock',  'Fox', false]]
+
+module PartsFactory
+
+  def self.build(config:,
+                  part_class: Parts,
+                  parts_classL Parts)
+
+    parts_class.new(
+      config.collect{ |part_config|
+        part_class.new(
+          name:        part_config[0],
+          description: part_config[1],
+          needs_spare: part_config.fetch(2,true))})
+  end
+end
+
+puts PartsFactory.build(config: road_config).inspect
+
+
+puts PartsFactory.build(config: mountain_config).inspect
 
