6c8feb31013636ea7b162d1bf5da9e664a16919b
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-07-01 17:12:14 -0700

    adding class wheel and gear

--- designing_cost_effective_tests.rb
+++ designing_cost_effective_tests.rb
@@ -3,4 +3,35 @@
 
 # Refactoring is the process of changing a software system in such a way that it does not alter the external behavior of the code yet improves the internal structure.
 
+class Wheel
+  attr_reader :rim, :size
+  def initialize(rim, size)
+    @rim  = rim
+    @size = size
+  end
 
+  def diameter
+    rim + (tire * 2)
+  end
+
+end
+
+class Gear
+  attr_reader :changring, :cog, :rim, :tire
+
+  def initialize(changring, cog, rim, tire)
+    @changring = changring
+    @cog       = cog
+    @rim       = rim
+    @tire      = tire
+  end
+
+  def gear_inches
+    ratio * Wheel.new(rim, tire).diameter
+  end
+
+  def raio
+    changring / cog.to_f
+  end
+
+end
